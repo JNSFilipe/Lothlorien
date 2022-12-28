19f19aca3cfc2f41cd5eda073a68d8dcf38db227
JNSFilipe <jose.filipe@ieee.org>
2022-12-28 21:09:25 +0000

    Implemented Random Forests

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@ mod metrics;
 mod algebra;
 mod heart_wood;
 mod galadh;
+mod taur;
 
 use rand::Rng;
 
 use crate::metrics::accuracy;
 use crate::algebra::argmax;
 use crate::galadh::Galadh;
+use crate::taur::Taur;
 
 #[cfg(test)]
 mod tests {
@@ -38,13 +40,14 @@ mod tests {
 
     #[test]
     fn accuracy_with_2d_threshold() {
+        let fixed_trheshold = 15.0;
         let mut rng = rand::thread_rng();
 
         let mut x = Vec::new();
         let mut y = Vec::new();
         for _ in 0..1000 {
             let sample = vec![rng.gen_range(0.0..50.0), rng.gen_range(0.0..50.0)];
-            let label = if sample[0] + sample[1] < 15.0 { 0 } else { 1 };
+            let label = if sample[0] + sample[1] < fixed_trheshold { 0 } else { 1 };
             x.push(sample);
             y.push(label);
         }
@@ -58,7 +61,7 @@ mod tests {
     }
 
     #[test]
-    fn print_test() {
+    fn print_tree_test() {
         let mut rng = rand::thread_rng();
 
         let mut x = Vec::new();
@@ -75,5 +78,27 @@ mod tests {
         tree.print();
 
         assert!(true);
+    }
+
+    #[test]
+    fn test_random_forests() {
+        let fixed_trheshold = 15.0;
+        let mut rng = rand::thread_rng();
+
+        let mut x = Vec::new();
+        let mut y = Vec::new();
+        for _ in 0..1000 {
+            let sample = vec![rng.gen_range(0.0..50.0), rng.gen_range(0.0..50.0)];
+            let label = if f64::powi(sample[0], 2) + f64::powi(sample[1], 3) < fixed_trheshold { 0 } else { 1 };
+            x.push(sample);
+            y.push(label);
+        }
+
+        let mut forest = Taur::new(100);
+        forest.sow(&x, &y, true);
+
+        let y_hat = forest.predict(&x);
+
+        assert!(accuracy(&y.as_slice(), &y_hat.as_slice()) > 95.0);
     }
 }
